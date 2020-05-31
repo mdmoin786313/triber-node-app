3e4de49296b13dc3292cca7875fcdb82f8e40b68
Mohammed Moin Ghadiyali <mdmoin786313@gmail.com>
2020-06-01 02:41:30 +0530

    mongo

--- config/mongoose.config.ts
+++ config/mongoose.config.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let dev_db_url = 'mongodb://localhost:27017/reacttest';
+let dev_db_url = 'mongodb+srv://moin:moin@1234@cluster0-xx5gk.mongodb.net/test?retryWrites=true&w=majority';
 
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true }).then(() => {
